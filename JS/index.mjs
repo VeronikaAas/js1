83289929e0fa9ed99ee291b41e91ea5573d6f808
Veronika Aas <veronikaaas@live.no>
2024-02-20 08:29:53 +0100

    Small save

--- JS/index.mjs
+++ JS/index.mjs
@@ -7,17 +7,47 @@
 import { API_GAMES_URL } from "./constants.mjs"
 import { doFetch } from "./doFetch.mjs"
 
+/*
+<div class="game-wrapper">
+    <div class="game-container">
+        <h3>Assassin</h3>
+        <div class ="game-price-container">
+        <div>Discounted Price: 13.49</div>
+        </div>
+    </div>
+</div>
+*/
+
+
 function generateGameHtml(game) {
     // Returns game HTML
-    Document.createElement
-    console.log(game);
+    const gameWrapper = document.createElement('div');
+    gameWrapper.classList.add('game-wrapper');
+
+    const gameWrapper = document.createElement('div');
+    gameWrapper.classList.add('game-container');
+
+    const heading = document.createElement('h3');
+    heading.textContent = game.title;
+
+    const gamePriceContainer = document.createElement('div');
+    gamePriceContainer.classList.add('')
+
+    const gamePrice = document.createElement('div');
+    gamePrice.textContent = game.price;
+
+    const gameDiscountedPrice = document.createElement('div');
+    gameDiscountedPrice.textContent = game.discountedPrice;
+
+    gamePriceContainer.append(gamePrice, gameDiscountedPrice);
+    return gameContainer;
+
 }
 
 function displayGames(games) {
     console.log(games);
     games.forEach((game) => {
         const gameHtml = generateGameHtml(game);
-        // generate HTML for the game
         
     });
 
